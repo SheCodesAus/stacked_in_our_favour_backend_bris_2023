40a7f1ebf7dfa875ec7e062a0c259d44da7b881c
yara-fe <yarafe95@gmail.com>
2023-10-13 11:31:09 +1000

    Created POST method for Events

--- winstack/events/serializers.py
+++ winstack/events/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from django.apps import apps
 
-class EventSerializer(serializers.Serializer):
+class EventSerializer(serializers.ModelSerializer):
     class Meta: 
         model = apps.get_model('events.Event')
         fields = '__all__'
+
--- winstack/events/views.py
+++ winstack/events/views.py
@@ -5,7 +5,15 @@ from .serializers import EventSerializer
 
 class EventList(APIView):
 
+    # Handles GET request
     def get(self,request):
         events = Event.objects.all()
         serializer = EventSerializer(events, many=True)
         return Response(serializer.data)
+    
+    # Handles POST request
+    def post(self, request):
+        serializer = EventSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
