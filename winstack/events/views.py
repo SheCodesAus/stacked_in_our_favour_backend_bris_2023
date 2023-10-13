b7373eb31249f6d10e8dce1325def8b0d495c87a
yara-fe <yarafe95@gmail.com>
2023-10-13 12:51:42 +1000

    Created view to handle singe event instance

--- winstack/events/views.py
+++ winstack/events/views.py
@@ -2,7 +2,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Event
 from .serializers import EventSerializer
+from django.http import Http404
+from rest_framework import status
 
+# Handle all events
 class EventList(APIView):
 
     # Handles GET request
@@ -13,7 +16,22 @@ class EventList(APIView):
     
     # Handles POST request
     def post(self, request):
-        serializer = EventSerializer(data=request.data)
+        serializer = EventSerializer(data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_202_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Handle single-event view
+class EventDetail(APIView):
+    
+    def get_object(self, pk):
+        try:
+            return Event.objects.get(pk=pk)
+        except Event.DoesNotExist:
+            raise Http404
+    
+    def get(self,request, pk):
+        event = self.get_object(pk)
+        serializer = EventSerializer(event)
+        return Response(serializer.data)
