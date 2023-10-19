601fe8f354d3380176b3c28e6620205d1e8854d1
yara-fe <yarafe95@gmail.com>
2023-10-20 07:57:14 +1000

    moved rm command after shebang line

--- winstack/run.sh
+++ winstack/run.sh
@@ -1,5 +1,5 @@
-rm -rf /dbvol/db.sqlite3
 #!/usr/bin/env bash
+rm -rf /dbvol/db.sqlite3
 python manage.py migrate
 python manage.py createsuperuser --no-input
 gunicorn --bind :8000 --workers 1 winstack.wsgi
